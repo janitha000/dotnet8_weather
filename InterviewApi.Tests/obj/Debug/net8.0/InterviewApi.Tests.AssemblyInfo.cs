fc8b425bcffc31281592c91e6ba85ef7ab81181f
Janitha Tennakoon <janitha@randomsoftware.net>
2026-08-10 16:54:37 +0530

    outbox implementation

--- InterviewApi.Tests/obj/Debug/net8.0/InterviewApi.Tests.AssemblyInfo.cs
+++ InterviewApi.Tests/obj/Debug/net8.0/InterviewApi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterviewApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d0b14754364411766dfbe6dd4abcbf299b8687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c0a4e0c398ce6d3ac7e917020e710543a418ebd")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterviewApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterviewApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
